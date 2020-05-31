cb8b4286f3772ca66585a8df8758dd4c1a622d6c
Themba-Dombo <themba.dombo@gmail.com>
2020-05-31 23:33:14 +0200

    completed the logic for the add function

--- spec/string_calculator_spec.js
+++ spec/string_calculator_spec.js
@@ -1,51 +1,82 @@
-const {add} = require("../src/string_calculator.js")
+const { add } = require("../src/string_calculator.js");
 
-describe("Add", ()=>{
-    
-    it("should return for empty string",()=>{
-        expect(add("")).toBe(0)
-    });
+describe("Add", () => {
+  it("should return 0 for empty string", () => {
+    expect(add("")).toBe(0);
+  });
 
-    it("should return interger value of string",()=>{
-        expect(add("1")).toBe(0)
-    });
+  it("should return interger value of string", () => {
+    expect(add("1")).toBe(1);
+  });
 
-    it("should return added interger values",()=>{
-        expect(add("1,1")).toBe(0)
-    });
+  it("should return added interger values", () => {
+    expect(add("1,1")).toBe(2);
+  });
 
-    it("should return added total of multiple string values",()=>{
-        expect(add("1,2,3,4")).toBe(10)
-    });
+  it("should return added total of multiple string values", () => {
+    expect(add("1,2,3,4")).toBe(10);
+  });
 
+  it("should handle new line between integers", () => {
+    expect(add("1\n2,3")).toBe(6);
+  });
 
+  it("should handle different delimeters symbols", () => {
+    expect(add("//;\n1;2")).toBe(3);
+  });
 
-    it("should handle new lines between integers",()=>{
-        expect(add("1\n2,3")).toBe(6)
-    });
+  it("should handle differen delimeters numeric", () => {
+    expect(add("//4\n142")).toBe(3);
+  });
 
-    it("should handle different delimeters",()=>{
-        expect(add("//;\n1;2")).toBe(3)
-    });
+  it("should handle differen delimeters alphabetic", () => {
+    expect(add("//df\n1df2")).toBe(3);
+  });
 
-    it("should not allow negative numbers",()=>{
-        expect(()=>{add("-1,-2,3,4")}).toThrow(new Error("negatives not allowed"))
-    });
+  it("should not allow negative numbers", () => {
+    expect(() => add("-1,-2,3,4")).toThrowError("negatives not allowed -1,-2");
+  });
 
+  it("should ignore integers greater or equal to 1000", () => {
+    expect(add("//;\n1000,1;2")).toBe(3);
+  });
 
+  it("should handle delimiters of any length", () => {
+    expect(add("//***\n1***2***3")).toBe(6);
+  });
+  it("should handle different delimiters of any length", () => {
+    expect(add("//[(-_-')][%]\n1(-_-')2%3")).toBe(6);
+  });
 
-    it("should ignore integers greater or equal to 1000",()=>{
-        expect(add("//;\n1000,1;2")).toBe(3)
-    });
+  it("should handle different delimiters of any length", () => {
+    expect(add("//[***][%%%]\n1***2%%%3")).toBe(6);
+  });
 
-    it("should handle delimiters of any length",()=>{
-        expect(add("//[(-_-')][%]\n1(-_-')2%3")).toBe(6)
-    });
+  it("should handle different delimiters of any length", () => {
+    expect(add("//[:D][%]\n1:D2%3")).toBe(6);
+  });
 
-    it("should return error for invalid input",()=>{
-        expect(()=>{add("   //;\n1000,1;2")}).toThrow(new Error("invalid input"))
-    });
+  it("should handle different delimiters of any length", () => {
+    expect(add("//[abc][777][:(] \n1abc27773:(1")).toBe(7);
+  });
 
+  it("should handle different delimiters of any length", () => {
+    expect(add("//[abc][7be][689] \n1abc27be36891")).toBe(7);
+  });
 
+  it("should return error for strings ending with other symbols", () => {
+    expect(() => add("//;\n1000;1;2;")).toThrowError("invalid input");
+  });
 
+  it("should return error for strings beginning with special characters", () => {
+    expect(() => add("   //;\n1000,1;2")).toThrowError("invalid input");
+  });
+
+  it("should return error for strings beginning numeric characters", () => {
+    expect(() => add("1,2,3//;\n1000,1;2")).toThrowError("invalid input");
+  });
+
+  it("should return error for strings beginning alphabetic characters", () => {
+    expect(() => add("a,B,c//;\n1000,1;2")).toThrowError("invalid input");
+  });
 });
